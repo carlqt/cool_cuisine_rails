c802df5c6b9c5aeb80eac4e75702dd2fc5f1bf5c
Carl Tablante <carl.q.tablante@gmail.com>
2019-03-16 14:33:52 +0800

    Added dependencies and created Product migration

---
+++ db/migrate/20190316063312_create_meals.rb
@@ -0,0 +1,12 @@
+class CreateMeals < ActiveRecord::Migration[5.2]
+  def change
+    create_table :meals do |t|
+      t.integer :price_cents
+      t.string :name
+      t.text :description
+      t.text :image_url
+
+      t.timestamps
+    end
+  end
+end
