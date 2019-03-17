69e345943954fb5117a5ec5c495a94570f3dc0df
Carl Tablante <carl.q.tablante@gmail.com>
2019-03-17 15:19:38 +0800

    Add categories column

---
+++ db/migrate/20190317071531_add_categories_to_meals.rb
@@ -0,0 +1,5 @@
+class AddCategoriesToMeals < ActiveRecord::Migration[5.2]
+  def change
+    add_column :meals, :categories, :string
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+categories = %w(
+  'sides',
+  'drinks',
+  'regular',
+)
+
 10.times do
   Meal.create(
     name: Faker::Coffee.blend_name,
     description: Faker::Coffee.notes,
     price_cents: Faker::Number.between(1000, 5000),
-    image_url: Faker::LoremPixel.image("450x300", false, "food")
+    image_url: Faker::LoremPixel.image("450x300", false, "food"),
+    categories: categories.sample,
   )
 end
